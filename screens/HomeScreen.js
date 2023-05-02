be2acaf62294a28499e1e1e4273cec1527c18e91
Lucas Henrique Araujo Vasconcelos <85376643+Lucas-dev-br@users.noreply.github.com>
2023-05-02 18:25:09 -0300

    feat: header section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,9 @@ import React, { useLayoutEffect } from "react";
 // Navigation
 import {useNavigation} from "@react-navigation/native"
 
+// HeroIcons
+import { ChevronDownIcon as ChevronDownIconOutiline, UserIcon } from "react-native-heroicons/outline"
+
 const HomeScreen = () => {
 
   const navigation = useNavigation();
@@ -15,19 +18,23 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className='mt-10'>
       <Text className="text-red-500">
 
         {/* Header */}
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image source={{
             uri: 'https://links.papareact.com/wru'
-          }} className='h-7 w-7 bg-gray-300 p-4 roundend-full'/>
-        </View>
+          }} className='h-7 w-7 bg-gray-300 p-4 rounded-full'/>
 
         <View>
           <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
-          <Text className='font-bold text-xl'>Current Location</Text>
+          <Text className='font-bold text-xl'>Current Location
+            <ChevronDownIconOutiline className="mx-5"  size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB"/>
         </View>
       </Text>
     </SafeAreaView>
